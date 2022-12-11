ebc33b13b272d28599bf0c3168f3f0ed17edfa73
migyeong <pratt3343@gmail.com>
2022-12-11 23:35:59 +0900

    20221211_Again

---
+++ src/For/For_While.java
@@ -0,0 +1,17 @@
+package For;
+
+import java.util.Scanner;
+
+public class For_While {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		sc.close();
+		int n = sc.nextInt();
+		for(int i=1; i <=n ; i++ ) {
+			System.out.println(i);
+		}
+	}
+
+}
---
+++ src/For/Forstatement.java
@@ -0,0 +1,15 @@
+package For;
+
+public class Forstatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 5; //반복될 횟수 
+		
+		while (i!=0) {
+			System.out.println("I can do it");
+			i++;
+		}
+	}
+
+}
